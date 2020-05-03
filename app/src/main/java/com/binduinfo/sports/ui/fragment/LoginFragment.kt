2f6406b273c6d33dc7667d77d6f89612e2e08bda
Dap E-nnovative <anushraj.n@digiaplus.com>
2020-05-03 12:25:46 +0530

    Password Toggle issue fixed

--- app/src/main/java/com/binduinfo/sports/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/binduinfo/sports/ui/fragment/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.binduinfo.sports.ui.fragment
 
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
