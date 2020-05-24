d8a6e8caf2d48376f5bd45805d8b608f5c1cab6e
Dap E-nnovative <anushraj.n@digiaplus.com>
2020-05-24 09:33:40 +0530

    Profile Picture and permission handle

--- app/src/main/java/com/binduinfo/sports/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/binduinfo/sports/ui/activity/HomeActivity.kt
@@ -1,8 +1,11 @@
 package com.binduinfo.sports.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -14,15 +17,15 @@ class HomeActivity : BaseActivity() {
     override fun uiHandle() {
 
     }
-
+    private lateinit var navController:NavController
+    private  var fragment: Fragment? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         hideToolbar()
         setContentView(R.layout.activity_home)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        if(!::navController.isInitialized)
+        navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_profile
@@ -31,4 +34,15 @@ class HomeActivity : BaseActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(fragment == null)
+         fragment = this.supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                ?.childFragmentManager?.fragments?.get(0)
+
+        fragment?.onActivityResult(requestCode, resultCode, data)
+
+    }
+
 }
--- app/src/main/java/com/binduinfo/sports/ui/fragment/profile/ProfileFragment.kt
+++ app/src/main/java/com/binduinfo/sports/ui/fragment/profile/ProfileFragment.kt
@@ -1,15 +1,26 @@
 package com.binduinfo.sports.ui.fragment.profile
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.binduinfo.sports.R
+import com.binduinfo.sports.util.cropimage.CropImage
+import com.binduinfo.sports.util.cropimage.CropImageView
+import com.bumptech.glide.Glide
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
@@ -29,4 +40,63 @@ class ProfileFragment : Fragment() {
 //        })
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        profile_pic.setOnClickListener {
+            Dexter.withActivity(requireActivity()).withPermissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    report?.let {
+                        if (it.areAllPermissionsGranted()) {
+                            CropImage.activity(null).setGuidelines(CropImageView.Guidelines.ON)
+                                .setMaxCropResultSize(1240, 1240)
+                                .setMinCropResultSize(820, 820)
+                                .start(
+                                    requireActivity(),
+                                    CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE
+                                )
+                        }else{
+                            Toast.makeText(requireContext(), "Enable Permission", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    token?.let {
+                        token.continuePermissionRequest()
+                    }
+                }
+
+            }).check()
+//            CropImage.activity(null).setGuidelines(CropImageView.Guidelines.ON)
+//                .setMaxCropResultSize(1240,1240)
+//                .setMinCropResultSize(820, 820)
+//                .start(requireActivity(), CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
+            /*.start(requireContext(), CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)*/
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("requestCode", requestCode.toString())
+        Log.d("data", data.toString())
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                    val result = CropImage.getActivityResult(data)
+                    Glide.with(requireActivity()).asBitmap().load(result.uri).into(profile_pic)
+                }
+            }
+        } else {
+            Toast.makeText(requireContext(), "Try again", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
